db76638fb652d9bca49b848f7d736a14922d5b95
vernellmiller <nezeey@gmail.com>
2021-09-20 18:12:45 -0400

    wip: adds starter files

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// // Variables and Constants
+
+const BASE_URL = "https://pokeapi.co/api/v2/"
+
+// Cached Elements References
+
+// Event Listeners
+
+// Functions
+function getData() {
+    // use $.ajax to fetch data from the space x api
+    // save the data locally
+    //transfer the data to the DOM
+}
