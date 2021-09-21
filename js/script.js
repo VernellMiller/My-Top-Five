b91ea8ad088b5ca13bc78633a536a27a9763afe2
vernellmiller <nezeey@gmail.com>
2021-09-21 13:30:20 -0400

    wip: adds base html, css, js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,7 @@
-// // Variables and Constants
-
-const BASE_URL = "https://pokeapi.co/api/v2/"
-
+// Constants and Variables
+ 
 // Cached Elements References
 
 // Event Listeners
 
 // Functions
-function getData() {
-    // use $.ajax to fetch data from the space x api
-    // save the data locally
-    //transfer the data to the DOM
-}
